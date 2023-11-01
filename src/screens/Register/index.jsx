2fe96fa1b78cdab7e407d06e976b753e537d443f
Hivna <hivna12@gmail.com>
2023-11-01 14:03:09 -0300

    chore: Separated Register file into index and style files

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -4,9 +4,9 @@ import {
   TouchableOpacity,
   TextInput,
   Text,
-  StyleSheet,
   KeyboardAvoidingView,
 } from "react-native";
+import { styles } from "./style"
 import { useNavigation } from "@react-navigation/native";
 import { LinearGradient } from "expo-linear-gradient";
 import Icon from "react-native-vector-icons/AntDesign";
@@ -144,129 +144,4 @@ const Register = () => {
     </LinearGradient>
   );
 };
-
-const styles = StyleSheet.create({
-  gradient: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  main: {
-    flex: 0.9,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-  },
-  container: {
-    flex: 3.2,
-    alignItems: "center",
-    width: "90%",
-    gap: 5,
-  },
-  containerTitle: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 32,
-  },
-  title: {
-    color: "#223843",
-    fontFamily: "black",
-    fontSize: 40,
-    letterSpacing: -2,
-  },
-  subTitle: {
-    color: "#fff",
-    fontFamily: "light",
-    fontSize: 16,
-  },
-  label: {
-    marginRight: "auto",
-    marginLeft: 48,
-    marginTop: 8,
-    fontFamily: "thin",
-    fontSize: 14,
-    color: "#fff",
-  },
-  viewInput: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 24,
-    borderWidth: 1,
-    borderColor: "#fff",
-    borderRadius: 20,
-    width: "90%",
-    height: 64,
-  },
-  input: {
-    marginTop: 18,
-    backgroundColor: "transparent",
-    marginBottom: 15,
-    color: "#fff",
-    fontFamily: "light",
-    fontSize: 17,
-  },
-  emailIcon: {
-    marginLeft: 10,
-  },
-  userIcon: {
-    marginLeft: 10,
-  },
-  lockedIcon: {
-    marginLeft: 15,
-    marginBottom: 5,
-  },
-  section: {
-    width: "80%",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 20,
-    marginTop: 52,
-  },
-  checkbox: {
-    borderColor: "#fff",
-    backgroundColor: "#fff",
-    marginBottom: 2,
-  },
-  paragraph: {
-    fontFamily: "light",
-    fontSize: 12,
-    color: "#fff",
-  },
-  acessButton: {
-    flexDirection: "row",
-    marginTop: 24,
-    marginBottom: 5,
-    width: "90%",
-    height: 48,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#223843",
-    borderRadius: 20,
-  },
-  acess: {
-    marginHorizontal: 64,
-    marginLeft: 90,
-    fontFamily: "black",
-    fontSize: 24,
-    color: "#fff",
-  },
-  loginButton: {
-    borderWidth: 1,
-    borderColor: "#fff",
-    paddingHorizontal: 36,
-    paddingVertical: 10,
-    borderRadius: 8,
-    marginTop: 48,
-  },
-  login: {
-    fontFamily: "medium",
-    fontSize: 16,
-    color: "#fff",
-  },
-});
-//#0DAB76
-//#223843
-
 export default Register;
