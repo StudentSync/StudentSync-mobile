d9bf55ce30951b058ac0f6998913f080205d15ad
Hivna <hivna12@gmail.com>
2023-11-27 15:40:43 -0300

    feat: added Group card component

--- src/screens/Group/index.jsx
+++ src/screens/Group/index.jsx
@@ -8,59 +8,47 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/AntDesign";
 import Icon2 from "react-native-vector-icons/Ionicons";
-import { styles }  from "./style";
+import { styles } from "./style";
 import { COLORS } from "../../utils/Colors";
 
-
 const Group = () => {
-
-  
- 
   return (
-      <KeyboardAvoidingView style={styles.background}>
-        <View style={styles.container}>
-
-          <View style={styles.containerNavigation}>
-
-            <View style={styles.containerNavigation}>
-              <Icon
-                  name="addusergroup"
-                  size={24}
-                  color={COLORS.primary}
-                  style={styles.iconGroup}
-                />
-            </View>
-            <View style={styles.containerTitle}>
-              <Text style={styles.title}>Turmas</Text>
-            </View>
-            <View style={styles.positionAddIcon}>
-                <Icon2
-                    name = "add-circle-outline"
-                    size={40}
-                    color={COLORS.redTwo}
-                    style={styles.iconGroup}
-                />
-            </View>
-
-          </View>
-
-          <View style={styles.containerGroup}>
-
-            <View>
-                    
-                <Text style={styles.nameGroup}> Isso </Text>
-                <View style={styles.cardGroup}>
-                    <Text style={styles.textGroup}>
-                        Isso
-                    </Text>
-                </View>
-
-            </View>
-            
-          </View>
-          
+    <KeyboardAvoidingView style={styles.background}>
+      
+      <View style={styles.containerNavigation}>
+        <TouchableOpacity>
+          <Icon
+            name="addusergroup"
+            size={24}
+            color={COLORS.primary}
+            style={styles.iconGroup}
+          />
+        </TouchableOpacity>
+        
+          <Text style={styles.title}>Turmas</Text>
+        
+        <View>
+          <Icon2
+            name="add-circle-outline"
+            size={40}
+            color={COLORS.redTwo}
+            style={styles.iconGroup}
+          />
         </View>
-      </KeyboardAvoidingView>
+      </View>
+
+      <View style={styles.containerGroup}>
+        
+        <View style={styles.cardGroup}>
+          <Text style={styles.titleGroup}> Title </Text>
+          <Text style={styles.textGroup}> Participantes </Text>
+        </View>
+
+
+
+      </View>
+      
+    </KeyboardAvoidingView>
   );
 };
 export default Group;
